4d67fb2a872bb13f1a77230247c7960a506c6dce
Bruno Martins <brunooliveira8430@hotmail.com>
2022-02-08 23:24:31 -0300

    Exercicio em execução

---
+++ modulo3/rend-condicional-form/src/components/Etapa2.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Etapa2() {
+  return (
+   <div></div>
+  );
+}
+
+export default App;
---
+++ modulo3/rend-condicional-form/src/components/Etapa3.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Etapa3() {
+  return (
+   <div></div>
+  );
+}
+
+export default App;
