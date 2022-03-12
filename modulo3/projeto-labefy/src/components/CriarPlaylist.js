01efaab861d08555d32f3accad2f92d64f8555a5
Bruno Martins <brunooliveira8430@hotmail.com>
2022-03-11 21:13:53 -0300

    Site ja cria playlist, consegue retormar todas playlist criadas

--- modulo3/projeto-labefy/src/components/CriarPlaylist.js
+++ modulo3/projeto-labefy/src/components/CriarPlaylist.js
@@ -1,17 +1,49 @@
+import axios from 'axios'
 import React from 'react'
 
 
 class CriarPlaylist extends React.Component {
+  state = {
+    inputname: ''
+  }
+handleInputName = (event) => {
+  this.setState({inputname: event.target.value})
+}
+
+createPlaylist = () => {
+  const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists'
+  const body = {
+    name: this.state.inputname
+  }
+
+  axios
+  .post(url, body, {
+    headers: {
+      Authorization: 'bruno-martins-gebru'
+    }
+  })
+  .then(res => {
+    alert(`Playlist ${this.state.inputname} criada com sucesso.`)
+    this.setState({inputname: ''})
+  })
+  .catch(err => alert(err.response.data.message))
+}
 
 
- render(){
+
+ render() {
+
   return (
     <div>
       <h1>
           Criar Playlist
       </h1>
-      <input placeholder='Insira nome'/>
-      <button>Criar</button>
+      <input
+       placeholder='Insira nome' 
+       value={this.state.inputname} 
+       onChange={this.handleInputName}
+      />
+      <button onClick={this.createPlaylist}>Criar</button>
     </div>
   )
  }
--- modulo3/projeto-labefy/src/components/VisualizarPlaylist.js
+++ modulo3/projeto-labefy/src/components/VisualizarPlaylist.js
@@ -1,14 +1,70 @@
 import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+const PlaylistCard = styled.div`
+display: flex;
+justify-content: space-between;
+margin: 10px;
+padding: 10px;
+width: 400px;
+`
+
 
 class VisualizarPlaylist extends React.Component {
+state = {
+  allPlaylist: []
+}
+
+  componentDidMount() {
+    this.getAllPlaylists()
+  }
+
+  getAllPlaylists = () => {
+    const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists'
+
+    axios
+    .get(url, {
+      headers: {
+        Authorization: 'bruno-martins-gebru'
+      }
+    })
+    .then(res => {
+      this.setState({allPlaylist: res.data.result.list})
+    })
+    .catch(err => console.error(err.response))
+  }
+
+  deletePlaylist = (playlistId) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`
+    axios
+    .delete(url, {
+      headers: {
+        Authorization: 'bruno-martins-gebru'
+      }
+    })
+    .then(res => {
+      alert('Playlist Deletada.')
+      this.getAllPlaylists()
+    })
+    .catch(err => console.log(err.response))
+  }
 
 
  render(){
+  const renderAllPlaylists = this.state.allPlaylist.map((playlist) => {
+    return <PlaylistCard key={playlist.id}>
+      <li>{playlist.name}</li>
+      <button onClick={() => this.deletePlaylist(playlist.id)}>Apagar</button>
+    </PlaylistCard>
+  })
+
   return (
     <div>
       <h1>
        Visualizar Playlist
       </h1>
+      <h3>{renderAllPlaylists}</h3>
     </div>
   )
  }
