a608f1f6f92949c1a3af02e35eeb98169ba5d7e1
Bruno Martins <brunooliveira8430@hotmail.com>
2022-04-07 22:04:51 -0300

    executando api

--- modulo4/projeto-labex/src/pages/AdminHomePage.js
+++ modulo4/projeto-labex/src/pages/AdminHomePage.js
@@ -1,16 +1,66 @@
+import axios from "axios"
 import React, {useState, useEffect} from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { goBack, goCreateTripPage, goTripDetailsPage } from "../routes/coordinator"
+import useRequestData from "../hooks/useRequestData";
 
 export default function AdminHomePage() {
  const navigate = useNavigate()
+
  const params = useParams()
- useEffect(()=>{
-    const token = localStorage.getItem("token")
-    if(token === null) {
-        AdminHomePage(navigate)
+
+ const listTrips = useRequestData()
+
+ const deletar = (delID) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labeX/bruno-martins-gebru/trips/${delID}`
+        const headers = {
+                    headers: {
+                   auth: localStorage.getItem("token")
+      }
     }
-     },[navigate])
+            
+        axios
+    .delete(url, headers)
+    .then(res => {
+      alert('Viagem Deletada.')
+      
+    })
+    .catch(err => console.log(err.response))
+  }
+
+    const trips = listTrips.map((trip)=> {
+        return <div>
+        <p>{trip.name}</p>
+        <button onClick={()=> deletar(trip.id)}>Apagar</button>
+        </div>
+    })
+
+//  useEffect(()=> {
+//      const URL = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/bruno-martins-gebru/trip/BjbMS1Bmqhcn1PleJYRM"
+//     const headers = {
+//         headers: {
+//         auth: localStorage.getItem("token")
+// //     }
+// //     }
+
+//     axios
+//     .get(URL, headers)
+//     .then((res)=>{
+//         console.log(res.data)
+//     })
+//     .catch((err)=>{
+//         console.log("erro", err.response)
+//     })
+//     },[])
+
+
+
+//  useEffect(()=>{
+//     const token = localStorage.getItem("token")
+//     if(token === null) {
+//         AdminHomePage(navigate)
+//     }
+//      },[navigate])
 
 
 
@@ -20,9 +70,7 @@ export default function AdminHomePage() {
             <button onClick={()=>goBack(navigate)}>Voltar</button>
             <button onClick={()=>goCreateTripPage(navigate)}>Criar Viagem</button>
             <button onClick={()=>goBack(navigate)}>Sair</button>
-            <div>
-                <button onClick={goTripDetailsPage(navigate)}> Exemplo detalhes das viagens</button>
-            </div>
+            {trips}
             
         </div>
     )
--- modulo4/projeto-labex/src/pages/ListTripsPage.js
+++ modulo4/projeto-labex/src/pages/ListTripsPage.js
@@ -1,17 +1,30 @@
-import React, {useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import useRequestData from "../hooks/useRequestData";
 import { goBack, goApplicationFormPage } from "../routes/coordinator";
 
 export default function ListTripsPage() {
     const navigate = useNavigate()
+    const listTrips = useRequestData()
+console.log(listTrips)
 
-    
+    const trips = listTrips.map((trip)=> {
+        return <div>
+        <p>Nome:{trip.name}</p>
+        <p>Descrição: {trip.description}</p>
+        <p>Planeta: {trip.planet}</p>planet
+        <p>Duração: {trip.durationInDays}</p>
+        <p>Data: {trip.date}</p>
+        <p>====================================================================================</p>
+        </div>
+    })
     
     return(
         <div>
             <p>Para vermos todas as viagens</p>
             <button onClick={()=>goBack(navigate)}>Voltar</button>
             <button onClick={()=>goApplicationFormPage(navigate)}>Inscrever</button>
+            {trips}
         </div>
     )
 }
