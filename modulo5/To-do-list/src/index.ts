951ac95c94edff74a0877d9c7250024653cd7b4b
Bruno Martins <brunooliveira8430@hotmail.com>
2022-06-24 22:34:26 -0300

    Projeto To Do List

---
+++ modulo5/To-do-list/src/index.ts
@@ -0,0 +1,11 @@
+import { createUser, searchId } from './api';
+import { app } from "./app";
+import { Request, Response } from 'express'
+
+
+app.get('/ping', (req: Request, res: Response)=>{
+    res.send('Pong!')
+})
+app.get('/user/:id', searchId)
+
+app.post('/user', createUser)
