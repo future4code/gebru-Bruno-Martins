e7aaf3e71958a6bbccd8656fad7cc5347174e58e
Bruno Martins <brunooliveira8430@hotmail.com>
2022-08-18 17:29:21 -0300

    exercicio

--- modulo7/introducao-autenticacao/src/business/UserBusiness.ts
+++ modulo7/introducao-autenticacao/src/business/UserBusiness.ts
@@ -1,11 +1,9 @@
+import { HashManager } from './../services/HashManager';
 import { UserDatabase } from "../data/UserDatabase";
-import { CustomError, InvalidEmail, InvalidName } from "../error/customError";
-import {
-  UserInputDTO,
-  user,
-  EditUserInputDTO,
-  EditUserInput,
-} from "../model/user";
+import { CustomError, InvalidEmail, InvalidName, InvalidPassword } from "../error/customError";
+import {UserInputDTO, user, EditUserInputDTO, EditUserInput,} from "../model/user";
+
+const hashManager = new HashManager()
 
 export class UserBusiness {
   public createUser = async (input: UserInputDTO) => {
@@ -29,15 +27,52 @@ export class UserBusiness {
 
       const id: string = Date.now().toString();
 
+      const hashPassoword = await hashManager.generateHash(password)
+
       const user: user = {
         id,
         name,
         nickname,
         email,
-        password,
+        password: hashPassoword,
       };
       const userDatabase = new UserDatabase();
       await userDatabase.insertUser(user);
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public login = async (input: LoginInputDTO): Promise<string> => {
+    try {
+      const { email, password } = input;
+    
+      if (!email || !password) {
+        throw new CustomError(
+          400,
+          'Preencha os campos"email" e "password"'
+        );
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const user = await userDatabase.findUser(email);
+
+      if (!user) {
+        throw new UserNotFound()
+      }
+
+      const hashCompare = await hashManager.compareHash(password, user.password)
+
+      if(!hashCompare){ 
+        throw new InvalidPassword()
+      }
+
+      const token = tokenGenerator.generateToken(user.id)
+     
+      return token
     } catch (error: any) {
       throw new CustomError(400, error.message);
     }
